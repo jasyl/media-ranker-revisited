57818883423a76867c9719dc385d26595e252a69
Jasmine <jasylop@gmail.com>
2020-11-28 15:37:41 -0800

    removed username duplicatd validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
-
   validates :uid, uniqueness: { scope: provider }
-  validates :username, presence: true
+
 
 
   def self.build_from_github(auth_hash)
