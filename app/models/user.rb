cadf858b5180e411db582b453301cdc3706851c7
Jasmine <jasylop@gmail.com>
2020-11-28 13:35:17 -0800

    added build_from_github method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.username = auth_hash[:info][:nickname]
+    user.email = auth_hash[:info][:email]
+    user.provider = "github"
+    return user
+  end
 end
