a8b3c3b063a3e0acc4b29e55ce612b5dfd2a0bff
Jasmine <jasylop@gmail.com>
2020-11-29 11:47:39 -0800

    added logic to prevent different providers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,20 +10,26 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.auth"]
-    user = User.find_by(uid: auth_hash[:uid], provider: "github" )
-    if user
-      flash[:status] = :success
-      flash[:result_text] = "Logged in as returning user #{user.username}"
-    else
-      user = User.build_from_github(auth_hash)
-      if user.save
+    if auth_hash
+      user = User.find_by(uid: auth_hash["uid"], provider: "github" )
+      if user
         flash[:status] = :success
-        flash[:result_text] = "Logged in as new user #{user.username}"
-      else # save unsuccessful
-        flash[:status] = :failure
-        flash[:result_text] = "Could not create new user account. #{user.errors.full_messages.join(", ")}"
-        return redirect_to root_path
+        flash[:result_text] = "Logged in as returning user #{user.username}"
+      else
+        user = User.build_from_github(auth_hash)
+        if user.save
+          flash[:status] = :success
+          flash[:result_text] = "Logged in as new user #{user.username}"
+        else # save unsuccessful
+          flash[:status] = :failure
+          flash[:result_text] = "Could not create new user account. #{user.errors.full_messages.join(", ")}"
+          return redirect_to root_path
+        end
       end
+    else # handles case when auth_hash is nil?
+      flash[:status] = :failure
+      flash[:result_text] = "Could not log in, #{params[:provider]} is not a valid provider"
+      return redirect_to root_path
     end
     session[:user_id] = user.id
     return redirect_to root_path
