f334d394f7b7a3253612fba7605e102cf661d053
Jasmine <jasylop@gmail.com>
2020-11-29 11:34:30 -0800

    added in upvote tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -189,19 +189,64 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+
+      work = works(:album)
+      expect {
+        post upvote_path(work)
+      }.wont_differ "Vote.count"
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:result_text]).must_equal "You must log in to do that"
+
     end
 
     it "redirects to the work page after the user has logged out" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+      post logout_path
+      work = works(:album)
+      expect {
+        post upvote_path(work)
+      }.wont_differ "Vote.count"
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:result_text]).must_equal "You must log in to do that"
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+
+      work = works(:poodr)
+      expect {
+        post upvote_path(work)
+      }.must_differ "Vote.count", 1
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+
+      expect(flash[:status]).must_equal :success
+      expect(flash[:result_text]).must_equal "Successfully upvoted!"
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+
+      work = works(:album)
+      expect {
+        post upvote_path(work)
+      }.wont_differ "Vote.count"
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:result_text]).must_equal "Could not upvote"
     end
   end
 end
